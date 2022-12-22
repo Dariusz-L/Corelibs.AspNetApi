263ced315b52548380c9e64c0958e3129a52582e
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-12-22 21:40:15 +0100

    Add extensions for sending query with automapper

---
+++ Corelibs.AspNetApi/Corelibs.AspNetApi/Controllers/Extensions/MapSendAndGetResponseWithMapperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Common.Basic.Blocks;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Corelibs.AspNetApi.Controllers.Extensions
+{
+    public static class MapSendAndGetResponseWithMapperExtensions
+    {
+        public static Task<IActionResult> MapSendAndGetResponse<TAppQuery, TReturnValue>(this IMediator mediator, object query, IMapper mapper)
+            where TAppQuery : IQuery<Result<TReturnValue>>
+        {
+            var appQuery = mapper.Map<TAppQuery>(query);
+            return mediator.SendAndGetResponse(appQuery);
+        }
+    }
+}
