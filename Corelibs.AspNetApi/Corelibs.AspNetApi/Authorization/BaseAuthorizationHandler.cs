2894c043ded45aaef70b9d8013cad69b0ab377b4
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-03 22:28:40 +0100

    Split an authorization base class

---
+++ Corelibs.AspNetApi/Corelibs.AspNetApi/Authorization/BaseAuthorizationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Corelibs.AspNetApi.Authorization
+{
+    public abstract class BaseAuthorizationHandler<TRequirement> : AuthorizationHandler<TRequirement>
+        where TRequirement : IAuthorizationRequirement
+    {
+        protected override async Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            TRequirement requirement)
+        {
+            bool result = await HandleRequirementImplAsync(context, requirement);
+            if (result)
+                context.Succeed(requirement);
+            else
+                context.Fail();
+        }
+
+        protected abstract Task<bool> HandleRequirementImplAsync(
+            AuthorizationHandlerContext context,
+            TRequirement requirement);
+    }
+}
--- Corelibs.AspNetApi/Corelibs.AspNetApi/Authorization/ResourceAuthorizationHandler.cs
+++ Corelibs.AspNetApi/Corelibs.AspNetApi/Authorization/ResourceAuthorizationHandler.cs
@@ -3,6 +3,7 @@ using Common.Basic.Collections;
 using Common.Basic.DDD;
 using Common.Basic.Repository;
 using Corelibs.Basic.Architecture.DDD;
+using Corelibs.Basic.Collections;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -10,62 +11,56 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Corelibs.AspNetApi.Authorization
 {
-    public class ResourceAuthorizationHandler<TResource> : AuthorizationHandler<SameOwnerRequirement<TResource>>
+    public class ResourceAuthorizationHandler<TResource> : BaseAuthorizationHandler<SameOwnerRequirement<TResource>>
         where TResource : IEntity, IOwnedEntity
     {
         private readonly IServiceProvider _serviceProvider;
-        private  IRepository<TResource> _resourceRespository;
 
         public ResourceAuthorizationHandler(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
-        protected override async Task HandleRequirementAsync(
+        protected override async Task<bool> HandleRequirementImplAsync(
             AuthorizationHandlerContext context,
             SameOwnerRequirement<TResource> requirement)
         {
             var userID = context.User.GetUserID();
             if (userID.IsNullOrEmpty())
-            {
-                context.Fail();
-                return;
-            }
+                return false;
             
             if (context.Resource is not HttpContext httpContext)
-            {
-                context.Fail();
-                return;
-            }
+                return false;
 
             using (var scope = _serviceProvider.CreateScope())
             {
-                _resourceRespository = scope.ServiceProvider.GetRequiredService<IRepository<TResource>>();
+                var resourceRespository = scope.ServiceProvider.GetRequiredService<IRepository<TResource>>();
 
                 var routeValues = httpContext.GetRouteData().Values;
                 var routeValue = routeValues.LastOrDefault(kv => kv.Key.ToLower().Contains("id"));
                 if (routeValue.Key.IsNullOrEmpty() || (routeValue.Value as string).IsNullOrEmpty())
-                {
-                    context.Fail();
-                    return;
-                }
+                    return false;
 
                 var resourceID = routeValue.Value as string;
+                if (!resourceID.IsID())
+                    return false;
 
                 var result = new Result();
-                var resource = await _resourceRespository.Get(resourceID, result);
-                
-                if (userID == resource.OwnerID)
-                    context.Succeed(requirement);
-                else
-                    context.Fail();
+                var resource = await resourceRespository.Get(resourceID, result);
+                if (resource == null)
+                    return false;
+
+                if (userID != resource.OwnerID)
+                    return false;
+
+                return true;
             }
         }
     }
 
     public class SameOwnerRequirement<TResource> : IAuthorizationRequirement 
         where TResource : IEntity, IOwnedEntity
-    { }
+    {}
 
     public static class ResourceAuthorizationExtensions
     {
@@ -75,8 +70,6 @@ namespace Corelibs.AspNetApi.Authorization
             var resourceName = typeof(TResource).Name;
             options.AddPolicy(AuthPolicies.Edit + resourceName, policy =>
                 policy.Requirements.Add(new SameOwnerRequirement<TResource>()));
-
-            //services.AddSingleton<IAuthorizationHandler, ResourceAuthorizationHandler<TResource>>();
         }
     }
 }
